f2c396e386d1062e0474a901adc9d969643146ca
vladshein <vlshein1@gmail.com>
2026-04-12 14:13:07 +0300

    fixed critical output issue, added gitignore, added outputs

--- lesson-5/outputs.tf
+++ lesson-5/outputs.tf
@@ -8,4 +8,18 @@ output "dynamodb_table_name" {
   value       = module.s3_backend.dynamodb_table_name
 }
 
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
 
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
+}
